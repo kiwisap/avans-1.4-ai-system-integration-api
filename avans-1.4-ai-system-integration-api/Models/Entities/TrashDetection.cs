c8cb1eea7b150928efb5639c5fb0a92aad2565fd
Yosin <rasulov.yosin@gmail.com>
2026-06-16 13:28:40 +0200

    model sensoring data added

---
+++ avans-1.4-ai-system-integration-api/Models/Entities/TrashDetection.cs
@@ -0,0 +1,31 @@
+﻿namespace avans_1._4_ai_system_integration_api.Models.Entities;
+
+public enum TrashType
+{
+    PLASTIC,
+    PAPIER_KARTON,
+    GLAS,
+    BLIK,
+    GROFVUIL,
+    ELEKTRISCHE_APPARATEN,
+    REST
+}
+
+public class TrashDetection
+{
+    public int Id { get; set; }
+
+    public double CameraLatitude { get; set; }
+    public double CameraLongitude { get; set; }
+
+    public double TrashLatitude { get; set; }
+    public double TrashLongitude { get; set; }
+
+    public DateTime PhotoTakenAtUtc { get; set; }
+
+    public double TemperatureCelsius { get; set; }
+
+    public TrashType Type { get; set; }
+
+    public bool Statiegeld { get; set; }
+}
